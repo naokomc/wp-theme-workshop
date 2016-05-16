3f99de668a165c416abc797c2c05e4a967ee03c0
miya0001 <miya@wpist.me>
2016-05-16 16:42:11 +0900

    change php code to template tag

--- wpmeetup-may-2016/wpmeetup-tokyo-done/index.php
+++ wpmeetup-may-2016/wpmeetup-tokyo-done/index.php
@@ -3,24 +3,18 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<?php
-	if ( have_posts() ) : while ( have_posts() ) : the_post();
-?>
-		<header class="entry-header">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>>"><?php the_title(); ?></a></h2>
-		</header>
-		<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-<?php 
-        the_content();
-    endwhile;
-    else :
-        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-    endif;
-?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<header class="entry-header">
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>>"><?php the_title(); ?></a></h2>
+			</header>
+			<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+			<?php the_content(); ?>
+		<?php endwhile; else: ?>
+			<?php _e( 'Sorry, no posts matched your criteria.', 'textdomain' ); ?>
+		<?php endif; ?>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
-<?php
-	get_sidebar();
-	get_footer();
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
