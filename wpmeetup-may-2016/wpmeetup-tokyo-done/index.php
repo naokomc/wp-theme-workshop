5c247baa52817e4721daf3d817a3b9b08b948fe6
miya0001 <miya@wpist.me>
2016-05-16 17:05:27 +0900

    fixed typo

--- wpmeetup-may-2016/wpmeetup-tokyo-done/index.php
+++ wpmeetup-may-2016/wpmeetup-tokyo-done/index.php
@@ -5,7 +5,7 @@
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<header class="entry-header">
-				<h2 class="entry-title"><a href="<?php the_permalink(); ?>>"><?php the_title(); ?></a></h2>
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			</header>
 			<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
 			<?php the_content(); ?>
